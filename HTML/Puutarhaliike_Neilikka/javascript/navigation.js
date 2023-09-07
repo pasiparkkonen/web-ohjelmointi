b7f0d7aef52f0cc961c4451ceecc5ad54188aa14
pasiparkkonen <pasi.parkkonen1974@gmail.com>
2023-09-08 00:41:38 +0300

    New files

---
+++ HTML/Puutarhaliike_Neilikka/javascript/navigation.js
@@ -0,0 +1,14 @@
+// navigation.js
+document.getElementById('nav-container').innerHTML = `
+    <ul>
+        <li><a href="index.html">Etusivu</a></li>
+        <li><a href="products.html">Tuotteet</a></li>
+        <li><a href="stores.html">Myymälät</a></li>
+        <li><a href="about.html">Tietoa meistä</a></li>
+        <li><a href="contact.html">Ota yhteyttä</a></li>
+    </ul>
+`;
+
+
+//<li><a href="services/service1.html">Service 1</a></li>
+//<li><a href="services/service2.html">Service 2</a></li>
---
+++ HTML/Puutarhaliike_Neilikka/javascript/pnavigation.js
@@ -0,0 +1,13 @@
+document.getElementById('product-nav-container').innerHTML = `
+    <ul>
+        <li><a href="index.html">Etusivu</a></li>
+        <li><a href="products.html">Tuotteet</a></li>
+        <li><a href="stores.html">Myymälät</a></li>
+        <li><a href="about.html">Tietoa meistä</a></li>
+        <li><a href="contact.html">Ota yhteyttä</a></li>
+        <li><a href="indoor.html">Sisäkasvit</a></li>
+        <li><a href="outdoor.html">Ulkokasvit</a></li>
+        <li><a href="tools.html">Työkalut</a></li>
+        <li><a href="plantCare.html">Kasvien hoito</a></li>
+    </ul>
+`;
