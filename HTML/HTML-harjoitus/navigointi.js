6b222c08594ddfa455b1c470c30f94a02ed9f62d
pasiparkkonen <pasi.parkkonen1974@gmail.com>
2023-09-13 09:05:30 +0300

    Eka toteutus

---
+++ HTML/HTML-harjoitus/navigointi.js
@@ -0,0 +1,10 @@
+document.getElementById('navigation-container').innerHTML = `
+    <ul>
+        <img src="Kuvat/Onlineshopicon.png" width="102px" height="52px" id="Logo" alt="Logo image"/>
+        <li><a href="Nettikauppa.html">Rekisteröinti</a></li>
+        <li><a href="Kuvagalleriasivu.html">Kuvagalleria</a></li>
+        <li><a href="Profiilisivu.html">Profiili</a></li>
+        <li><a href="Verkkosivu.html">Verkkosivu</a></li>
+        <li><a href="Vieritys.html">Vieritys</a></li>
+    </ul>
+`;
---
+++ HTML/HTML-harjoitus/valintajoukko.js
@@ -0,0 +1,6 @@
+document.getElementById('valintajoukko').innerHTML = `
+    <a href="#"><img src="Kuvat/sampopankki.png" alt="Sampo"></a>
+    <a href="#"><img src="Kuvat/nordea.png" alt="Nordea"></a>
+    <a href="#"><img src="Kuvat/op.png" alt="Op"></a>
+    <a href="#"><img src="Kuvat/aktia.png" alt="Aktia"></a>
+`;
