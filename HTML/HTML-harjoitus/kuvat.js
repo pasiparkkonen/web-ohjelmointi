fa95ff7c9ab85d96474e476c810fb0c4f85f21cf
pasiparkkonen <pasi.parkkonen1974@gmail.com>
2023-09-13 11:04:52 +0300

    galleria add

---
+++ HTML/HTML-harjoitus/kuvat.js
@@ -0,0 +1,8 @@
+document.getElementById('kuvagalleria').innerHTML = `
+<a><img src="Kuvat/Kuva1.jpg" alt="Kuva1"></a>
+<a><img src="Kuvat/Kuva2.jpg" alt="Kuva2"></a>
+<a><img src="Kuvat/Kuva3.jpg" alt="Kuva3"></a>
+<a><img src="Kuvat/Kuva4.jpg" alt="Kuva4"></a>
+<a><img src="Kuvat/Kuva5.jpg" alt="Kuva5"></a>
+<a><img src="Kuvat/Kuva6.jpg" alt="Kuva6"></a>
+`;
